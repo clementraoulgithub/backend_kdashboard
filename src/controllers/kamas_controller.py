0780b3339e77dfe2bab361d6d30388e4012e561c
Clément RAOUL <craoul@groupeastek.fr>
2023-12-19 00:33:42 +0100

    update time by utc

--- src/controllers/kamas_controller.py
+++ src/controllers/kamas_controller.py
@@ -15,7 +15,8 @@ async def create_kamas_value(message: Kamas_Pydantic):
 
 @router.get("/today", responses={404: {"model": HTTPNotFoundError}})
 async def get_today_kamas(server: str):
-    today_start = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    # get the datetime but in utc
+    today_start = datetime.datetime.now(datetime.timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
     today_end = today_start + datetime.timedelta(days=1)
 
     return await Kamas.filter(timestamp__gte=today_start, timestamp__lt=today_end, server=server).all()
@@ -23,7 +24,7 @@ async def get_today_kamas(server: str):
 
 @router.get("/yesterday", responses={404: {"model": HTTPNotFoundError}})
 async def get_yesterday_kamas(server: str):
-    today_start = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    today_start = datetime.datetime.now(datetime.timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
     yesterday_start = today_start - datetime.timedelta(days=1)
 
     return await Kamas.filter(timestamp__gte=yesterday_start, timestamp__lt=today_start, server=server).all()
