8b127c3da4c17d1cdf5b20077dc9f6e0b3036303
Clément RAOUL <craoul@groupeastek.fr>
2023-12-23 17:26:38 +0100

    add day/week and month query for gettings kamas values

--- src/controllers/kamas_controller.py
+++ src/controllers/kamas_controller.py
@@ -44,5 +44,24 @@ async def get_yesterday_kamas(server: str):
 
 
 @router.get("/kamas", responses={404: {"model": HTTPNotFoundError}})
-async def get_kamas(server: str):
-    return await Kamas.filter(server=server).all()
+async def get_kamas(server: str, scope: str):
+    today_start = datetime.datetime.now(datetime.timezone.utc).replace(
+        hour=0, minute=0, second=0, microsecond=0
+    )
+    if scope == "day":
+        today_end = today_start + datetime.timedelta(days=1)
+        return await Kamas.filter(
+            timestamp__gte=today_start, timestamp__lt=today_end, server=server
+        ).order_by("timestamp")
+    elif scope == "week":
+        week_start = today_start - datetime.timedelta(days=today_start.weekday())
+        week_end = week_start + datetime.timedelta(days=7)
+        return await Kamas.filter(
+            timestamp__gte=week_start, timestamp__lt=week_end, server=server
+        ).order_by("timestamp")
+    else:
+        month_start = today_start.replace(day=1)
+        month_end = month_start + datetime.timedelta(days=32)
+        return await Kamas.filter(
+            timestamp__gte=month_start, timestamp__lt=month_end, server=server
+        ).order_by("timestamp")
